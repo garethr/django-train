3374764d89c392e4266f142db29cf98d546cd1df
Gareth Rushgrove <gareth@morethanseven.net>
2008-12-21 12:15:36 +0000

    dropped the Akistmet handling in favour of manual moderation. Mainly because it didn't appear to work properly

--- src/train/signals.py
+++ src/train/signals.py
@@ -1,35 +1,7 @@
 from django.contrib.comments.signals import comment_was_posted
 
 def on_comment_was_posted(sender, comment, request, *args, **kwargs):
-
-    from django.contrib.sites.models import Site
-    from django.conf import settings
-
-    try:
-        from akismet import Akismet
-    except:
-        return
-
-    ak = Akismet(
-        key=settings.AKISMET_API_KEY,
-        blog_url='http://%s/' % Site.objects.get(pk=settings.SITE_ID).domain
-    )
-
-    if ak.verify_key():
-        data = {
-            'user_ip': request.META.get('REMOTE_ADDR', '127.0.0.1'),
-            'user_agent': request.META.get('HTTP_USER_AGENT', ''),
-            'referrer': request.META.get('HTTP_REFERER', ''),
-            'comment_type': 'comment',
-            'comment_author': comment.user_name.encode('utf-8'),
-        }
-
-        if ak.comment_check(comment.comment.encode('utf-8'), data=data, build_data=True):
-            comment.flags.create(
-                user=comment.content_object.author,
-                flag='spam'
-            )
-            comment.is_public = False
-            comment.save()
+    comment.is_public = False
+    comment.save()
 
 comment_was_posted.connect(on_comment_was_posted)
